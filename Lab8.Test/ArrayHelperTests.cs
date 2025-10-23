f8dc69adee18bb133f216fec6cdfa3dc1c4b228c
PhhHuynn <phuonghuyen5872@gmail.com>
2025-10-23 18:05:44 +0700

    first commit

---
+++ Lab8.Test/ArrayHelperTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8.Test
+{
+	internal class ArrayHelperTests
+	{
+		[Test]
+		public void FindMax_ShouldReturnMaximumValue()
+		{
+			int[] numbers = { 3, 7, 1, 9, 2 };
+			int result = ArrayHelper.FindMax(numbers);
+			Assert.AreEqual(9, result);
+		}
+
+		[Test]
+		public void FindMin_ShouldReturnMinimumValue()
+		{
+			int[] numbers = { 3, 7, 1, 9, 2 };
+			int result = ArrayHelper.FindMin(numbers);
+			Assert.AreEqual(1, result);
+		}
+
+		[Test]
+		public void FindMax_ShouldThrowException_WhenArrayEmpty()
+		{
+			int[] numbers = { };
+			Assert.Throws<ArgumentException>(() => ArrayHelper.FindMax(numbers));
+		}
+
+		[Test]
+		public void FindMin_ShouldThrowException_WhenArrayIsNull()
+		{
+			int[] numbers = null;
+			Assert.Throws<ArgumentException>(() => ArrayHelper.FindMin(numbers));
+		}
+	}
+}
---
+++ Lab8/ArrayHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8
+{
+	public class ArrayHelper
+	{
+		public static int FindMax(int[] numbers)
+		{
+			if (numbers == null || numbers.Length == 0)
+				throw new ArgumentException("Array cannot be null or empty.");
+
+			int max = numbers[0];
+			foreach (var num in numbers)
+			{
+				if (num > max)
+					max = num;
+			}
+			return max;
+		}
+
+		public static int FindMin(int[] numbers)
+		{
+			if (numbers == null || numbers.Length == 0)
+				throw new ArgumentException("Array cannot be null or empty.");
+
+			int min = numbers[0];
+			foreach (var num in numbers)
+			{
+				if (num < min)
+					min = num;
+			}
+			return min;
+		}
+	}
+}
